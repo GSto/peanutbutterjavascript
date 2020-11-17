2fd1970c1ebb1e09c2149ea55e2cd1d945d7ed47
Glenn Stovall <glennstovall@gmail.com>
2020-11-16 20:50:11 -0500

    fixes tag link.

--- _components/tag_list.js
+++ _components/tag_list.js
@@ -1,8 +1,9 @@
 import Link from 'next/link'
+import { slugify } from '_pb/transformers'
 
 export function Tag({ tag }) {
   return (
-    <Link href={`/tags/${tag}`}>
+    <Link href={`/tags/${slugify(tag)}`}>
       <a className="bg-purple-700 hover:bg-purple-600 text-xs text-white mr-2 p-1 tracking-wider cursor-pointer mt-1">
         {tag}
       </a>
