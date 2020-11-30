1a020f3b8565e2635abcecd4d77ce5f92e8c85c3
Glenn Stovall <glennstovall@gmail.com>
2020-11-30 18:21:43 -0500

    Fixes webpack warnings.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
     target: 'serverless',
     webpack: function(config) {
-        config.module.rules.push({ test: /\.md$/, use: 'raw-loader'})
+        config.module.rules.push({ test: /\.md$/i, use: 'raw-loader'})
+        config.module.rules.push({ test: /\.xml$/i, use: 'xml-loader'})
+        config.module.rules.push({ test: /\.(png|jpe?g|gif)$/i, use: 'file-loader'})
         return config
     }
 }
