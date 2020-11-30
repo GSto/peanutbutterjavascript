2e8d6638317c15c7c8373809ce04db3dd6d0c41c
Glenn Stovall <glennstovall@gmail.com>
2020-11-30 14:02:44 -0500

    Adds header to home page.

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,23 @@ import { getAllPosts, getConfig } from '@pb/post_query'
 export default function Index({ title, description, posts }) {
     return (
       <DefaultLayout title={title} description={description}>
-        <div className="max-w-4xl p-4 ml-auto mr-auto">
+        <div className="bg-black text-white w-full">
+          <div className="max-w-4xl px-4 py-24 mx-auto">
+            <p className="text-5xl tracking-wide">
+              JavaScript solutions for the real world
+            </p>
+            <p>
+              <a 
+                className="text-sm underline cursor-pointer" 
+                href="https://github.com/GSto/peanutbutterjavascript" 
+                target="_blank">
+                  View source
+              </a>
+            </p>
+
+          </div>
+        </div>
+        <div className="max-w-4xl p-4 mx-auto">
           <PostList posts={posts} />
         </div>
       </DefaultLayout>
