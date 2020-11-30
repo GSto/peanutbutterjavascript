4e00e83991dceb4d8bc2ba76b1db2cd9b22b7976
Glenn Stovall <glennstovall@gmail.com>
2020-11-30 15:11:38 -0500

    Removes post template.

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,34 @@
-import PostLayout from '@layouts/post'
+import DefaultLayout from '@layouts/default'
+import Head from 'next/head'
+import { PostDate } from '@components/post_list'
+import TagList from '@components/tag_list'
+import { Content, Title, StyledMarkdown, CTA } from '@components/theme'
 import { getPostBySlug, getAllPosts } from "@pb/post_query"
 
 export default function Post({ post }) {
+  const { title, content, published_at, tags } = post
   return (
-    <PostLayout post={post} />
+    <DefaultLayout>
+      <Head>
+        <title>{ title }</title>
+      </Head>
+      <Content>
+        <Title>{ title }</Title>
+        <div className="flex flex-row justify-center w-full mb-8">
+          <span className="mr-4">
+            <PostDate>{published_at}</PostDate>
+          </span>
+          <TagList tags={tags}></TagList>
+        </div>
+        <StyledMarkdown content={content} />
+        <CTA>
+          To get more JavaScript tips and news, you can follow me on Twitter: <a href="twitter.com/gsto" target="_blank" className="font-bold underline pl-1">@GSto</a>
+        </CTA>
+        <span className="text-sm">
+          Have a comment or question? <a href="https://github.com/GSto/peanutbutterjavascript/issues" target="_blank" className="underline cursor-pointer">Submit an issue.</a>
+        </span>
+      </Content>
+    </DefaultLayout>
   )
 }
 
