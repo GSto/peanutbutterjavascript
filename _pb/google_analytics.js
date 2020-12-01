6171f1d0390692cb88f7b92f79d55317abf6a541
Glenn Stovall <glennstovall@gmail.com>
2020-12-01 12:44:33 -0500

    Fixes bug when google analytics ID is not present.

--- _pb/google_analytics.js
+++ _pb/google_analytics.js
@@ -1,6 +1,7 @@
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export function trackPageview(url) {
+  if (!window.gtag) return
   window.gtag("config",process.env.GA_TRACKING_ID, {
     page_path: url,
   })
@@ -8,6 +9,7 @@ export function trackPageview(url) {
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export function trackEvent(action, event_category, event_label, value) {
+  if (!window.gtag) return
   window.gtag("event", action, {
     event_category, event_label, value
   })
