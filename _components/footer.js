e0fd9e1549b1c81beeed34ab69b116eb208165a6
Glenn Stovall <glennstovall@gmail.com>
2020-12-14 14:15:55 -0500

    Fixes navigation on mobile.

--- _components/footer.js
+++ _components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-row items-center bg-orange-700 text-white p-6 tracking-wider">
+    <footer className="flex flex-row items-center bg-orange-700 text-white p-6 tracking-wider w-full">
       <p>©2020 by <a href="https://glennstovall.com" className="underline">Glenn Stovall</a></p>
     </footer>
   )
